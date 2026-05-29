d74ec85a7abe30c40d767281c22897fcbabd230e
Zarif Atai <z.atai@protonmail.com>
2026-05-29 14:22:58 +0200

    Added 2.12 code

--- src/resp.rs
+++ src/resp.rs
@@ -1,10 +1,11 @@
-// Finished up and till 2.11
+// Finished up and till 2.12
 
 use crate::resp_result::{RESPError, RESPLength, RESPResult};
 use std::fmt;
 
 #[derive(Debug, PartialEq)]
 pub enum RESP {
+    Array(Vec<RESP>),
     BulkString(String),
     Null,
     SimpleString(String),
@@ -13,6 +14,16 @@ pub enum RESP {
 impl fmt::Display for RESP {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let data = match self {
+            Self::Array(data) => {
+                let mut output = String::from("*");
+                output.push_str(format!("{}\r\n", data.len()).as_str());
+
+                for elem in data.iter() {
+                    output.push_str(elem.to_string().as_str());
+                }
+
+                output
+            }
             Self::BulkString(data) => format!("${}\r\n{}\r\n", data.len(), data),
             Self::Null => String::from("$-1\r\n"),
             Self::SimpleString(data) => format!("+{}\r\n", data),
@@ -78,6 +89,7 @@ fn parser_router(
     match buffer[*index] {
         b'+' => Some(parse_simple_string),
         b'$' => Some(parse_bulk_string),
+        b'*' => Some(parse_array),
         _ => None,
     }
 }
@@ -134,6 +146,30 @@ fn parse_bulk_string(buffer: &[u8], index: &mut usize) -> RESPResult<RESP> {
     *index += 2;
 
     Ok(RESP::BulkString(data))
+}
+
+fn parse_array(buffer: &[u8], index: &mut usize) -> RESPResult<RESP> {
+    resp_remove_type('*', buffer, index)?;
+
+    let length = resp_extract_length(buffer, index)?;
+
+    if length < 0 {
+        return Err(RESPError::IncorrectLength(length));
+    }
+
+    let mut data = Vec::new();
+
+    for _ in 0..length {
+        match parser_router(buffer, index) {
+            Some(parse_func) => {
+                let array_element: RESP = parse_func(buffer, index)?;
+
+                data.push(array_element);
+            }
+            None => return Err(RESPError::Unknown),
+        }
+    }
+    Ok(RESP::Array(data))
 }
 
 #[cfg(test)]
@@ -390,5 +426,50 @@ mod tests {
 
         assert_eq!(output, RESP::BulkString(String::from("OK")));
         assert_eq!(index, 8);
+    }
+
+    #[test]
+    fn test_parse_array() {
+        let buffer = "*2\r\n+OK\r\n$5\r\nVALUE\r\n".as_bytes();
+        let mut index: usize = 0;
+
+        let output = parse_array(buffer, &mut index).unwrap();
+
+        assert_eq!(
+            output,
+            RESP::Array(vec![
+                RESP::SimpleString(String::from("OK")),
+                RESP::BulkString(String::from("VALUE")),
+            ])
+        );
+        assert_eq!(index, 20);
+    }
+
+    #[test]
+    fn test_parse_array_invalid_length() {
+        let buffer = "*-1\r\n+OK\r\n$5\r\nVALUE\r\n".as_bytes();
+        let mut index: usize = 0;
+
+        let error = parse_array(buffer, &mut index).unwrap_err();
+
+        assert_eq!(error, RESPError::IncorrectLength(-1));
+        assert_eq!(index, 5);
+    }
+
+    #[test]
+    fn test_bytes_to_resp_array() {
+        let buffer = "*2\r\n+OK\r\n$5\r\nVALUE\r\n".as_bytes();
+        let mut index: usize = 0;
+
+        let output = bytes_to_resp(buffer, &mut index).unwrap();
+
+        assert_eq!(
+            output,
+            RESP::Array(vec![
+                RESP::SimpleString(String::from("OK")),
+                RESP::BulkString(String::from("VALUE")),
+            ])
+        );
+        assert_eq!(index, 20);
     }
 }
